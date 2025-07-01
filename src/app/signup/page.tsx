4cef7625ba2cf0bc19e80c9faa627815026263f5
TIRTHANKAR DASGUPTA <tirthankardasgupta913913@gmail.com>
2025-07-01 08:37:04 +0000

    use proper google logo in the button

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -15,10 +15,14 @@ import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, up
 import { Loader2 } from "lucide-react";
 
 const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.85 3.18-1.73 4.1-1.02 1.02-2.62 1.62-4.88 1.62-3.86 0-6.99-3.14-6.99-7s3.13-7 6.99-7c2.16 0 3.63.89 4.49 1.69l2.6-2.6C16.84 3.3 14.86 2.5 12.48 2.5c-5.48 0-9.98 4.5-9.98 9.98s4.5 9.98 9.98 9.98c5.48 0 9.74-3.88 9.74-9.98 0-.6-.05-1.2-.15-1.78Z" fill="currentColor"/>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" role="img" {...props}>
+        <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+        <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.42-4.55H24v9.12h12.92c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+        <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24s.92 7.54 2.56 10.78l7.97-6.19z"></path>
+        <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+        <path fill="none" d="M0 0h48v48H0z"></path>
     </svg>
-  );
+);
 
 export default function SignupPage() {
   const router = useRouter();
