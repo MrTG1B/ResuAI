c8a621d3d522d7b272f5105afa2db32a90bf0fec
TIRTHANKAR DASGUPTA <tirthankardasgupta913913@gmail.com>
2025-07-01 16:37:40 +0000

    us e this as a logo and do no alter anything of the logo just change the

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,60 +1,39 @@
 import React from 'react';
 
 export const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg viewBox="0 0 180 70" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <g clipPath="url(#clip0_logo)">
-        <circle cx="62.5" cy="10.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="47.5" cy="22.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="77.5" cy="22.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="62.5" cy="34.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="92.5" cy="10.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="107.5" cy="22.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="92.5" cy="34.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="122.5" cy="10.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="137.5" cy="22.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="122.5" cy="34.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="152.5" cy="10.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="167.5" cy="22.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="152.5" cy="34.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="47.5" cy="46.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="77.5" cy="46.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="62.5" cy="58.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="107.5" cy="46.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="92.5" cy="58.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="137.5" cy="46.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="122.5" cy="58.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="167.5" cy="46.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="152.5" cy="58.5" r="6.5" fill="#FF9E0D"/>
-        <circle cx="70" cy="4" r="2" fill="#FF9E0D"/>
-        <circle cx="55" cy="16" r="2" fill="#FF9E0D"/>
-        <circle cx="85" cy="16" r="2" fill="#FF9E0D"/>
-        <circle cx="70" cy="28" r="2" fill="#FF9E0D"/>
-        <circle cx="100" cy="4" r="2" fill="#FF9E0D"/>
-        <circle cx="115" cy="16" r="2" fill="#FF9E0D"/>
-        <circle cx="100" cy="28" r="2" fill="#FF9E0D"/>
-        <circle cx="130" cy="4" r="2" fill="#FF9E0D"/>
-        <circle cx="145" cy="16" r="2" fill="#FF9E0D"/>
-        <circle cx="130" cy="28" r="2" fill="#FF9E0D"/>
-        <circle cx="160" cy="4" r="2" fill="#FF9E0D"/>
-        <circle cx="175" cy="16" r="2" fill="#FF9E0D"/>
-        <circle cx="160" cy="28" r="2" fill="#FF9E0D"/>
-        <circle cx="55" cy="40" r="2" fill="#FF9E0D"/>
-        <circle cx="85" cy="40" r="2" fill="#FF9E0D"/>
-        <circle cx="70" cy="52" r="2" fill="#FF9E0D"/>
-        <circle cx="115" cy="40" r="2" fill="#FF9E0D"/>
-        <circle cx="100" cy="52" r="2" fill="#FF9E0D"/>
-        <circle cx="145" cy="40" r="2" fill="#FF9E0D"/>
-        <circle cx="130" cy="52" r="2" fill="#FF9E0D"/>
-        <circle cx="175" cy="40" r="2" fill="#FF9E0D"/>
-        <circle cx="160" cy="52" r="2" fill="#FF9E0D"/>
-    </g>
-    <text x="5" y="55" fontFamily="Inter, sans-serif" fontSize="48" fontWeight="900" letterSpacing="-2" fill="#111111">
+    <svg viewBox="0 0 183 70" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <rect width="183" height="70" fill="black"/>
+    <circle cx="25" cy="39" r="3" fill="#FF9E0D"/>
+    <circle cx="33" cy="55" r="5" fill="#FF9E0D"/>
+    <circle cx="45" cy="65" r="2" fill="#FF9E0D"/>
+    <circle cx="50" cy="10" r="6" fill="#FF9E0D"/>
+    <circle cx="48" cy="25" r="8" fill="#FF9E0D"/>
+    <circle cx="65" cy="22" r="3" fill="#FF9E0D"/>
+    <circle cx="63" cy="38" r="6" fill="#FF9E0D"/>
+    <circle cx="58" cy="53" r="8" fill="#FF9E0D"/>
+    <circle cx="78" cy="8" r="4" fill="#FF9E0D"/>
+    <circle cx="80" cy="30" r="9" fill="#FF9E0D"/>
+    <circle cx="75" cy="50" r="3" fill="#FF9E0D"/>
+    <circle cx="88" cy="63" r="5" fill="#FF9E0D"/>
+    <circle cx="100" cy="15" r="8" fill="#FF9E0D"/>
+    <circle cx="98" cy="45" r="9" fill="#FF9E0D"/>
+    <circle cx="110" cy="60" r="3" fill="#FF9E0D"/>
+    <circle cx="120" cy="8" r="3" fill="#FF9E0D"/>
+    <circle cx="118" cy="28" r="6" fill="#FF9E0D"/>
+    <circle cx="125" cy="58" r="6" fill="#FF9E0D"/>
+    <circle cx="135" cy="18" r="2" fill="#FF9E0D"/>
+    <circle cx="140" cy="35" r="8" fill="#FF9E0D"/>
+    <circle cx="142" cy="51" r="4" fill="#FF9E0D"/>
+    <circle cx="155" cy="8" r="5" fill="#FF9E0D"/>
+    <circle cx="160" cy="25" r="9" fill="#FF9E0D"/>
+    <circle cx="158" cy="60" r="7" fill="#FF9E0D"/>
+    <circle cx="175" cy="45" r="3" fill="#FF9E0D"/>
+    <circle cx="20" cy="5" r="2" fill="#FF9E0D"/>
+    <circle cx="90" cy="4" r="2" fill="#FF9E0D"/>
+    <circle cx="130" cy="4" r="2" fill="#FF9E0D"/>
+    <circle cx="35" cy="15" r="2" fill="#FF9E0D"/>
+    <text y="55" x="5" fontFamily="Inter, sans-serif" fontSize="48" fontWeight="900" letterSpacing="-2" fill="white">
         <tspan>ResuAI</tspan>
     </text>
-    <defs>
-        <clipPath id="clip0_logo">
-            <rect width="180" height="70" fill="white"/>
-        </clipPath>
-    </defs>
   </svg>
 );
