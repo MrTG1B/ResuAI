082d5141be595164c000c644796727aeaee51044
TIRTHANKAR DASGUPTA <tirthankardasgupta913913@gmail.com>
2025-07-30 01:34:12 +0000

    fix this what happend

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -9,15 +9,10 @@ import { cn } from "@/lib/utils"
 const Tabs = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.Root>
->(({ className, orientation = "vertical", ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <TabsPrimitive.Root
     ref={ref}
-    orientation={orientation}
-    className={cn(
-        "flex",
-        orientation === 'vertical' && "flex-col sm:flex-row",
-        orientation === 'horizontal' && "flex-col",
-         className)}
+    className={cn("flex flex-col", className)}
     {...props}
   />
 ));
@@ -31,9 +26,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-auto items-start justify-start rounded-md bg-muted p-1 text-muted-foreground flex-shrink-0",
-      "[&[aria-orientation=vertical]]:flex-wrap [&[aria-orientation=vertical]]:sm:flex-col [&[aria-orientation=vertical]]:sm:h-auto",
-      "[&[aria-orientation=horizontal]]:flex-row",
+      "inline-flex h-auto items-center justify-start rounded-md bg-muted p-1 text-muted-foreground",
       className
     )}
     {...props}
@@ -48,7 +41,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-start w-full whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
       className
     )}
     {...props}
@@ -63,9 +56,7 @@ const TabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      "w-full mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      "[&[aria-orientation=vertical]]:mt-0 [&[aria-orientation=vertical]]:sm:ml-4",
-      "[&[aria-orientation=horizontal]]:mt-4",
+      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
       className
     )}
     {...props}
