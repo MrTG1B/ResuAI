e13c3d59ead4878cfb8c96c176983ce6a4a9d7ef
TIRTHANKAR DASGUPTA <tirthankardasgupta913913@gmail.com>
2025-09-21 09:24:35 +0000

    Error: AlertDialogTrigger is not defined
    src/app/aptitude-test/page.tsx

--- src/app/aptitude-test/page.tsx
+++ src/app/aptitude-test/page.tsx
@@ -18,11 +18,13 @@ import { Progress } from '@/components/ui/progress';
 import {
   AlertDialog,
   AlertDialogAction,
+  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { CreativeLoader } from '@/components/creative-loader';
 
