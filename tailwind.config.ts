0b09154b5683ffe139abfe1ba538a5252d00e362
TIRTHANKAR DASGUPTA <tirthankardasgupta913913@gmail.com>
2025-09-23 09:26:31 +0000

    Fix it it is not fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-
 import type {Config} from 'tailwindcss';
 
 export default {
@@ -11,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Open Sans"', 'sans-serif'],
-        heading: ['Poppins', 'sans-serif'],
+        sans: ['"Inter Tight"', 'sans-serif'],
+        heading: ['"Archivo Black"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
@@ -109,10 +108,23 @@ export default {
             transform: 'translateY(0)',
           },
         },
-        'wave': {
-            '0%': { backgroundPosition: '0% 50%' },
-            '50%': { backgroundPosition: '100% 50%' },
-            '100%': { backgroundPosition: '0% 50%' },
+        wave: {
+          '0%': {backgroundPosition: '0% 50%'},
+          '50%': {backgroundPosition: '100% 50%'},
+          '100%': {backgroundPosition: '0% 50%'},
+        },
+        wobble: {
+          '0%, 100%': {transform: 'rotate(0deg)'},
+          '25%': {transform: 'rotate(-3deg)'},
+          '75%': {transform: 'rotate(3deg)'},
+        },
+        gradient: {
+          '0%': {'background-position': '0% 50%'},
+          '50%': {'background-position': '100% 50%'},
+          '100%': {'background-position': '0% 50%'},
+        },
+        'brand-spin': {
+          to: {transform: 'rotate(360deg)'},
         },
       },
       animation: {
@@ -120,7 +132,10 @@ export default {
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in-down': 'fade-in-down 0.5s ease-out forwards',
         'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
-        'wave': 'wave 4s ease infinite',
+        wave: 'wave 4s ease infinite',
+        wobble: 'wobble 1s ease-in-out infinite',
+        gradient: 'gradient 3s ease infinite',
+        'brand-spin': 'brand-spin 1s linear infinite',
       },
     },
   },
